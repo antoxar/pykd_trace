38c033ed59d5611f5853ca5765ebe5ac4dd137d7
antoxar <blackzver@gmail.com>
2011-07-20 12:34:23 +0400

    Logging all child processes into file.
    BUG: Something strange with garbage collector  when you bp you make initial bp

--- pykdtrace.py
+++ pykdtrace.py
@@ -1,28 +1,28 @@
 __author__ = 'antoxar'
 from pykd import *
 import sys
-
+import time
 nt = loadModule( "nt" )
  
 class Logs:
-    def __init__(self, fileName):
-        fName = "c:\\temp\\%s.log" % (fileName[:-4])
+    def __init__(self):
+        fName = 'c:\\temp\\{0:>s}.log'.format(str(time.strftime("%a_%d_%b_%Y_%H_%M", time.gmtime())))
         dprintln( "%s" % fName)
         try:
             self.file = open( fName, 'w')
         except IOError as error:
-            dprintln("IOError {0:>s}".format(error.message))
+            dprintln('IOError {0:>s}'.format(error.message))
 
     def write( self, message ):
         self.file.write( message + "\n" )
 
     def __del__(self):
-        self.file.close ()
+        self.file.close()
 
 class BpDict:
     def __init__(self, CallbackPtrList, BpHandler):
         self.modDict = [bp(x, BpHandler) for x in CallbackPtrList()]
-
+        
     def set(self):
         for k in self.modDict:
             k.set()
@@ -31,68 +31,75 @@ class BpDict:
         for k in self.modDict:
             k.remove()
 
+class BpHandlers:
+
+    def __init__(self, name ):
+        self.bpobject = None
+        self.dropProc = list()
+        self.logs = Logs()
+        self.dr_name = name
+        self.bpobject = BpDict( GetSyscallList, self.SysCallbackHandler )
+        self.bpobject.rem()
+
+    def  __del__(self):
+        del self.bpobject
+        del self.logs
+
+    def GetCurrentProcess(self):
+        str = dbgCommand(".printf \"%x\n\", poi(poi(fs:[0x124])+0x50)")
+        return int(str, 16)
+    
+    def SysCallbackHandler(self):
+        pr_addr = self.GetCurrentProcess()
+        eprocess = typedVar("nt", "_EPROCESS", pr_addr )
+        if eprocess.UniqueProcessId in self.dropProc:
+            func = findSymbol(reg("eip"))
+            dprintln(repr(func))
+            self.logs.write(func)
+        return DEBUG_STATUS_GO
+
+    def CloseProcessHandler(self):
+        #dprintln( "CloseProcessHandler" )
+        pr_addr = ptrDWord(reg("esp") + 4)
+        eprocess = typedVar("nt", "_EPROCESS", pr_addr )
+        if eprocess.UniqueProcessId in self.dropProc:
+            self.dropProc.remove(eprocess.UniqueProcessId)
+            dprintln("Rem UniqueProcessId %x" % eprocess.UniqueProcessId)
+            self.bpobject.rem()
+        return DEBUG_STATUS_GO
+    
+    def CreateProcessHandler(self):
+        #dprintln( "CreateProcessHandler" )
+        pr_addr = reg("eax")
+        eprocess = typedVar("nt", "_EPROCESS", pr_addr )
+        fileName = ''.join( chr(i) for i in eprocess.ImageFileName).rstrip('\x00')
+        if eprocess.InheritedFromUniqueProcessId in self.dropProc:
+            self.dropProc.append(eprocess.UniqueProcessId)
+            dprintln("Add child %x" % eprocess.UniqueProcessId)
+        if self.dr_name in fileName:
+            self.dropProc.append(eprocess.UniqueProcessId)
+            dprintln("Add UniqueProcessId %x" % eprocess.UniqueProcessId)
+            #self.bpobject.set()
+        return DEBUG_STATUS_GO
+
+
 def GetSyscallList():
     serviceTableHeader = loadDWords( nt.KeServiceDescriptorTable, 4 )
     serviceTableStart = serviceTableHeader[0]
     serviceCount = serviceTableHeader[2]
     return loadPtrs( serviceTableStart, serviceCount )
 
-class BpHandlers:
-    def __init__(self, name ):
-        self.bpobject = None
-        self.dropProc = dict()
-        self.dr_name = name
-        self.bpobject = BpDict( GetSyscallList, self.SysCallbackHandler )
-        self.bpobject.rem()
+def start(name):
+    handlers = BpHandlers(name)
+    b1 = bp( nt.PspInsertProcess, handlers.CreateProcessHandler)
+    b2 = bp( nt.PspProcessDelete, handlers.CloseProcessHandler)
+    go()
+    del handlers
+    dprintln("stopped")
     
-    def _GetCurrentProcess(self):
-        str = dbgCommand(".printf \"%x\n\", poi(poi(fs:[0x124])+0x50)")
-        return int(str, 16)
-    
-    def SysCallbackHandler(self):
-        str = dbgCommand(".printf \"%x\n\", poi(poi(fs:[0x124])+0x50)")
-        pr_addr = int(str, 16)
-        eprocess = typedVar("nt", "_EPROCESS", pr_addr )
-        fileName = ''.join(( chr(a) for a in eprocess.ImageFileName))
-        dprintln(repr(fileName))
-        if self.dropProc.has_key(pr_addr):
-            func = findSymbol(reg("eip"))
-            dprintln(repr(func))
-            self.dropProc[pr_addr].write(func)
-        return DEBUG_STATUS_GO
-
-    def CloseProcessHandler(self):
-        dprintln( "CloseProcessHandler" )
-        pr_addr = ptrDWord(reg("esp") + 4)
-        eprocess = typedVar("nt", "_EPROCESS", pr_addr )
-        fileName = ''.join(( chr(a) for a in eprocess.ImageFileName))
-        dprintln("test")
-        if self.dr_name in fileName:
-            if self.dropProc.has_key(pr_addr):
-                del self.dropProc[pr_addr]
-                self.bpobject.rem()
-        return DEBUG_STATUS_GO
-    
-    def CreateProcessHandler(self):
-        dprintln( "CreateProcessHandler" )
-        pr_addr = reg("eax")
-        eprocess = typedVar("nt", "_EPROCESS", pr_addr )
-        fileName = ''.join(( chr(a) for a in eprocess.ImageFileName))
-        dprintln("test")
-        if self.dr_name in fileName:
-            self.dropProc[pr_addr] = Logs(fileName)
-            dprintln(repr(self.dropProc))
-            self.bpobject.set()
-        return DEBUG_STATUS_GO
-
-
 if __name__ == "__main__":
     if len(sys.argv) == 2:
-        handlers = BpHandlers(sys.argv[1])
-        b1 = bp( nt.PspInsertProcess, handlers.CreateProcessHandler)
-        b2 = bp( nt.PspProcessDelete, handlers.CloseProcessHandler)
-        go()
-        dprintln("stopped")
+        start(sys.argv[1])
     else:
         dprintln( "Bp Tracer" )
         dprintln( "Using "  + sys.argv[0] + " <PROCESS_NAME>" )
